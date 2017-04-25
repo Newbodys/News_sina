ca9acd9b568cb0a2b86a20ad624b057f0468b295
Newbodys <klier@163.com>
2017-04-25 21:36:37 +0800

    Update News_sina.py

--- News_sina/spiders/News_sina.py
+++ News_sina/spiders/News_sina.py
@@ -1,33 +1,24 @@
 import scrapy 
 import codecs
 from pymongo import MongoClient
-<<<<<<< HEAD
 from scrapy.selector import Selector
-=======
->>>>>>> 7f12d2cea5be2f00dcb819f599fdac0aa8837509
 
 client = MongoClient('localhost',27017)
 db = client.news
 
-<<<<<<< HEAD
+
 class News_sina(scrapy.Spider):
-=======
+
 class News_sina(scrapy.Spiderhttps):
->>>>>>> 7f12d2cea5be2f00dcb819f599fdac0aa8837509
 	name = "News_sina"
 	start_urls = []
 	url_base = "http://roll.news.sina.com.cn/s/channel.php?ch=01#col=89&spec=&type=&ch=01&k=&offset_page=0&offset_num=0&num=80&asc=&page="
 
-<<<<<<< HEAD
-	for i in xrange(1,3):
-=======
 	for i in xrange(1,10):
->>>>>>> 7f12d2cea5be2f00dcb819f599fdac0aa8837509
 		urls = url_base + str(i)
 		start_urls.append(urls)
 	
 	def parse(self,response):
-<<<<<<< HEAD
 		body_li = response.xpath('//li').extract()
 		for i in body_li:
 			#sel = Selector(text=i)
@@ -66,9 +57,3 @@ class News_sina(scrapy.Spiderhttps):
 	def parse_page(self,response):
 		news_content = response.xpath('//div[@class="article-a__content"]/p').extract()
 		db.news_list.update({"news_content_link":response.url},{'$set':{"news_content":news_content}})
-=======
-		channel = response.xpath('//span[@class="c_ch1"]/a/text()').extract()
-		title = response.xpath('//span[@class="c_tit"]/a/text()').extract()
-		time = response.xpath('//span[@class="c_time"]/text()').extract()
-		news_content_link = response.xpath('//span[@class="c_tit"]/a/@href').extract()
->>>>>>> 7f12d2cea5be2f00dcb819f599fdac0aa8837509
