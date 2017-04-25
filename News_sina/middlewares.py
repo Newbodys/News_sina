472dfdb132b041b8e931d3f68e72322515b3662b
Newbodys <klier@163.com>
2017-04-25 21:42:14 +0800

    Update middlewares.py

--- News_sina/middlewares.py
+++ News_sina/middlewares.py
@@ -2,7 +2,7 @@
 
 # Define here the models for your spider middleware
 #
-# See documentation in:
+# See documentations in:
 # http://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
 from scrapy import signals
